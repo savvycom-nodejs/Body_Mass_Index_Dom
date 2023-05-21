eb3457680a0ff5b0b81dc05f34b4e9f30c170ce5
Thanhyeu94 <lamthanh200294@gmail.com>
2023-05-21 16:44:29 +0700

    nop bai tap lan 1

---
+++ app.js
@@ -0,0 +1,32 @@
+function buttonCalculate(){
+    let height = document.getElementById("input-height").value;
+    let weight = document.getElementById("input-weight").value;
+    let resultNumber= document.getElementById("resultNumber").innerHTML=(parseFloat(weight)/ (parseFloat(height)* parseFloat(height))).toFixed(1);
+    let radioButton = document.querySelector('input[name="gender"]:checked');
+    let selectedValue= radioButton.value 
+    if(resultNumber<19.1 && selectedValue==='female'){
+        document.getElementById("resultText").innerHTML= "Gầy"
+    }if(resultNumber>=19.1 && resultNumber<=25.8 && selectedValue==='female'){
+        document.getElementById("resultText").innerHTML= "Bình thường"
+    }if(resultNumber>25.8 && resultNumber<=27.3 && selectedValue==='female'){
+        document.getElementById("resultText").innerHTML= "Thừa cân nhẹ"
+    }if(resultNumber>27.3 && resultNumber<=32.3 && selectedValue==='female'){
+        document.getElementById("resultText").innerHTML= "Trên lý tưởng"
+    }if(resultNumber>32.3 && selectedValue==='female'){
+        document.getElementById("resultText").innerHTML= "Béo phì"
+    }
+    if(resultNumber<20.7 && selectedValue==='male'){
+        document.getElementById("resultText").innerHTML= "Gầy"
+    }if(resultNumber>=20.7 && resultNumber<=26.4 && selectedValue==='male'){
+        document.getElementById("resultText").innerHTML= "Bình thường"
+    }if(resultNumber>26.4 && resultNumber<=27.8 && selectedValue==='male'){
+        document.getElementById("resultText").innerHTML= "Thừa cân nhẹ"
+    }if(resultNumber>27.8 && resultNumber<=31.1 && selectedValue==='male'){
+        document.getElementById("resultText").innerHTML= "Trên lý tưởng"
+    }else if(resultNumber>31.1 && selectedValue==='female'){
+        document.getElementById("resultText").innerHTML= "Béo phì"
+    }
+    document.getElementById("input-height").value=""
+    document.getElementById("input-weight").value=""
+
+}
